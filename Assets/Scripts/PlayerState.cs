b1b360e1ddaae06537174ec063102403723f063c
Noah García <noah@danielgarcia.info>
2022-05-01 16:08:38 -0500

    new player animation

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -1,10 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class PlayerState : ScriptableObject
+public class PlayerState : ScriptableObject, ISerializationCallbackReceiver
 {
-    public Vector2 facing;
-    public bool canMove = true;
+    public Vector2 initialFacing;
+    public bool initialCanMove = true;
+
+    [NonSerialized] public Vector2 facing;
+    [NonSerialized] public bool canMove = true;
+
+    public void OnAfterDeserialize()
+    {
+        facing = initialFacing; 
+        canMove = initialCanMove; 
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
+    }
 }
